c2172d59767b87d356206a42b99b10f5edacf25b
Pulkit Yadav <96831141+pulkit-yadav129@users.noreply.github.com>
2022-12-22 11:48:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Joke from "./Joke";
+import Stories from "./Stories";
+
+
+
+function App() {
+
+  const [userQuery,setUserQuery]=useState('');
+
+  const searchQuery=()=>{
+    window.open(`https://google.com/search?q=${userQuery}`,'_blank');
+  }
+  
+
+  const handleKeyPress=(event)=>{
+    if(event.key==='Enter'){
+      searchQuery();
+    }
+  }
+
+  const updateUserQuery=event=>{
+    console.log('userQuery', userQuery);
+
+    setUserQuery(event.target.value);
+  }
+
+
+  return (
+    <div className="App">
+      <h1>Hello Pulkit!</h1>
+      <div className="form">
+        <input 
+         value={userQuery}
+         onChange={updateUserQuery}
+         onKeyPress={handleKeyPress}
+         />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+      <hr/>
+      <Joke/>
+      <hr/>
+      <Stories/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Joke.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useFetch } from './hooks';
+
+function Joke(){
+
+    const joke=useFetch('https://official-joke-api.appspot.com/random_joke',{});
+
+    const {setup,punchline}=joke;
+    return (
+        <div>
+            <h3>Joke</h3>
+            <p>{setup}</p>
+            <p><em>{punchline}</em></p>
+        </div>
+    )
+}
+
+export default Joke;
